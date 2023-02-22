14cdb293e929ca3d0c77f76164a89568436a1d29
Jonathan Marshall <jmarshall@never.you.mind>
2023-02-23 09:42:12 +1300

    switch from xaringanBulider to renderthis

--- lectures/print_to_pdf.R
+++ lectures/print_to_pdf.R
@@ -1,9 +1,9 @@
 library(tidyverse)
-library(xaringanBuilder)
+library(renderthis)
 
-to_pdf <- 3:7
+which_lectures <- 8:9
 
-files = tibble(html = sprintf('lecture%02i.html', to_pdf))
+files = tibble(html = sprintf('lecture%02i.html', which_lectures))
 
 files %>% mutate(file = file.path('../lectures', html)) %>%
-  pull(file) %>% map(build_pdf)
+  pull(file) %>% map(to_pdf)
