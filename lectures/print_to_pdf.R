43ea4722a5fe243f72cf3ba532400479a93ae53e
Jonathan Marshall <jmarshall@never.you.mind>
2025-01-31 13:28:02 +1300

    removes a slide from lecture 9

--- lectures/print_to_pdf.R
+++ lectures/print_to_pdf.R
@@ -1,9 +1,9 @@
 library(tidyverse)
 library(renderthis)
 
-which_lectures <- 8:9
+which_lectures <- 8
 
 files = tibble(html = sprintf('lecture%02i.html', which_lectures))
 
-files %>% mutate(file = file.path('../lectures', html)) %>%
+files %>% mutate(file = file.path('lectures', html)) %>%
   pull(file) %>% map(to_pdf)
